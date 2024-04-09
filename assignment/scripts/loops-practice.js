80397cfae70b182670c06a4814661da8a70d1b03
Kalia Xiong <kaliaxyooj@gmail.com>
2024-04-08 21:11:18 -0500

    add console log strings

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -7,33 +7,33 @@ console.log('---- 1. For loops ----');
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
 for (let i = 0; i < 4; i++) {
-  console.log(i);
+  console.log('count 0-3:', i);
 }
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 for (i = 0; i < 6; i++) {
-  console.log(i);
+  console.log('count 0-5:',i);
 }
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 for (i = 3; i < 6; i++) {
-  console.log(i);
+  console.log('count 3-5:', i);
 }
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for (i = 0; i < 11; i += 2) {
-  console.log(i);
+  console.log('even numbers:', i);
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 for (i = 5; i > -1; i--) {
-  console.log(i);
+  console.log('count 5-0:', i);
 }
 
 
@@ -65,7 +65,7 @@ console.log('count from 0 to 5');
 
 let a = 0;
 while (a < 6) {
-  console.log(a);
+  console.log( '0-5:', a);
   a += 1;
 }
 
